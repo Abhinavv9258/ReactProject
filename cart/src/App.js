e4d846db9a3f471c949a1ff3f1b3fc8a8fbda288
Abhinavv9258 <abhinavv218@gmail.com>
2022-12-30 12:28:04 +0530

    cart

--- cart/src/App.js
+++ cart/src/App.js
@@ -5,10 +5,10 @@ import CartItem from './CartItem';
 function App() {
   return (
     <div className="App">
-      <h1>Cart</h1>
       <CartItem />
     </div>
   );
 }
+
 
 export default App;
--- cart/src/CartItem.js
+++ cart/src/CartItem.js
@@ -1,25 +1,48 @@
 import React from 'react';
+import "./index.css";
 import 'bootstrap/dist/css/bootstrap.css';
 
 // eslint-disable-next-line
 class CartItem extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            price:999,
+            title:'Phone',
+            qty:1,
+            img:''
+        }
+    }
     render() {
+        const {price,title,qty} = this.state;
         return (
             <div className="cart-item">
                 <div className="left-block">
-                    {/* <img/> */}
-                    <h1>helllo</h1>
+                    <img style={styles.image}/>
                 </div>               
                 <div className="right-block">
-                    <div>Phone</div>
-                    <div>Rs 999</div>
-                    <div>Qty: 1</div>
+                    <div style={{fontSize:25}}>{title}</div>
+                    <div style={{color:'#777'}}>Rs {price}</div>
+                    <div style={{color:'#777'}}>Qty: {qty}</div>
                     <div className="cart-item-actions">
                         {/* Button */}
+                        <img alt="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992651.png"/>
+                        <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992683.png"/>
+                        <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png"/>
                     </div>
                 </div>
             </div>
         );
     }
 }
+
+const styles = {
+  image:{
+    height: 150,
+    width: 150,
+    borderRadius: 5
+  }
+}
+
+
 export default CartItem;
